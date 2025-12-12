019048424605b43f7545e2a73aebdb8263eec563
Ekasestao <ekaitzcampo2002@gmail.com>
2025-12-12 08:47:57 +0100

    Changing JS format

--- PHP_Server_CRUD/view/assets/js/login.js
+++ PHP_Server_CRUD/view/assets/js/login.js
@@ -7,23 +7,23 @@ document.addEventListener("DOMContentLoaded", async () => {
       const username = document.getElementById("username").value;
       const password = document.getElementById("password").value;
 
-      const response = await fetch("../../api/Login.php", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ username, password }),
-      });
+      try {
+        const response = await fetch("../../api/Login.php", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        });
 
-      if (response.ok) {
         const result = await response.json();
 
-        localStorage.setItem("actualProfile", result.data);
-        window.location.href = "main.html";
-      } else if (response.status === 403) {
-        alert("Incorrect credentials.");
-      } else if (response.status === 500) {
-        console.log("Server error.");
-      }
+        if (response.ok) {
+          localStorage.setItem("actualProfile", result.data);
+          window.location.href = "main.html";
+        } else if (response.status === 403) {
+          alert(result.message);
+        }
+      } catch {}
     });
 });
