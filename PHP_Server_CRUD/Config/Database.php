e3f6b46dc5260e4c02bcfad83a684be08fb10624
Ekasestao <ekaitzcampo2002@gmail.com>
2026-01-08 10:31:20 +0100

    Modificando BD

--- PHP_Server_CRUD/Config/Database.php
+++ PHP_Server_CRUD/Config/Database.php
@@ -1,12 +1,14 @@
 <?php
-class Database {
+class Database
+{
     private $host = "localhost";
-    private $db_name = "CRUD_ADT";
+    private $db_name = "CRUD_DW";
     private $username = "root";
     private $password = "abcd*1234";
     private $conn;
 
-    public function getConnection() {
+    public function getConnection()
+    {
         $this->conn = null;
         try {
             $this->conn = new PDO(
@@ -21,6 +23,3 @@ class Database {
         return $this->conn;
     }
 }
-?>
-
-
